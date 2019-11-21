e9e7e7abe6c4b76ce51e6e627fc89c522472337b
Yifan <jameswu1212@gmail.com>
2019-11-21 17:04:05 +0800

    Update HW8, finish part a, b

--- HW8/main.py
+++ HW8/main.py
@@ -4,10 +4,11 @@ Yi-Fan Wu (R08921104)
 
 import cv2
 import numpy as np
+import random
 
 
 # Generate noise images with gaussian noise (amplitude)
-def gaussian_noise(amp):
+def gaussian_noise(img, amp):
     height, width = img.shape[:2]
     for h in range(height):
         for w in range(width):
@@ -15,22 +16,34 @@ def gaussian_noise(amp):
 
     return img
 
+# Generate noise images with salt and pepper
+def salt_and_pepper_noise(img, p):
+    height, width = img.shape[:2]
+    for h in range(height):
+        for w in range(width):
+            var = random.uniform(0, 1)
+            if var < p:
+                img[h, w] = 0
+            elif var > (1 - p):
+                img[h, w] = 255
+    
+    return img
+
+
+def noise_removal_with_box(img, size):
+
 
 def main():
-    print('1. Reading the image...')
+    print('Reading the image...')
     img = cv2.imread('lena.bmp', cv2.IMREAD_GRAYSCALE)
 
-    noise_img = gaussian_noise(np.copy(img), 10)
-    cv2.imwrite('a-gaussian_noise_10', noise_img)
+    print('Generating gaussian noise images with amptitude 30...')
     noise_img = gaussian_noise(np.copy(img), 30)
-    cv2.imwrite('a-gaussian_noise_30', noise_img)
+    cv2.imwrite('a-gaussian_noise_30.bmp', noise_img)
 
-    print('2. Downsampling...')
-    down_img = downsample(binary_img)
-
-    print('3. Thinning...')
-    thin_img = thinning(down_img)
-    cv2.imwrite('thinning.bmp', thin_img)
+    print('Generating salt and pepper noise images with probability 0.1...')
+    noise_img = salt_and_pepper_noise(np.copy(img), 0.1)
+    cv2.imwrite('b-salt_and_pepper_noise_0_1.bmp', noise_img)
     
 
 
