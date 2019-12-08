225c0b3d292d3bf2c98b3a1f83c3f3d39d0f8054
Yifan <jameswu1212@gmail.com>
2019-12-08 22:44:44 +0800

    Add HW9

---
+++ HW9/main.py
@@ -0,0 +1,16 @@
+""" Homework 9 - General Edge Detection
+Yi-Fan Wu (r08921104)
+"""
+
+import cv2
+import numpy as np
+
+
+
+def main():
+    print('1. Reading the image...')
+    img = cv2.imread('lena.bmp', cv2.IMREAD_GRAYSCALE)
+    
+
+if __name__ == '__main__':
+    main()
