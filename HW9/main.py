a3db97f6df8775052c592804d7358d1c8941620c
Yifan <jameswu1212@gmail.com>
2019-12-09 01:13:13 +0800

    Update HW8 (Robert's edge detection)

--- HW9/main.py
+++ HW9/main.py
@@ -4,29 +4,38 @@ Yi-Fan Wu (r08921104)
 
 import cv2
 import numpy as np
+import math
 
 def roberts_operator(img, threshold):
+    print('Robert\'s edge detection with threshold ' + str(threshold))
     height, width = img.shape[:2]
     img_result = np.copy(img)
 
     for h in range(height):
         for w in range(width):
-            right = img[h, w + 1] if w + 1 < width else 0
-            bottom = img[h + 1, w] if h + 1 < height else 0
-            right_bottom = img[h + 1, w + 1] if w + 1 < width and h + 1 < height else 0
+            right = int(img[h, w + 1]) if w + 1 < width else 0
+            bottom = int(img[h + 1, w]) if h + 1 < height else 0
+            right_bottom = int(img[h + 1, w + 1]) if w + 1 < width and h + 1 < height else 0
 
-            r1 = right_bottom - img[h, w]
+            r1 = right_bottom - int(img[h, w])
             r2 = bottom - right
+            mag = math.sqrt(r1 * r1 + r2 * r2)
+
+            if mag >= threshold:
+                img_result[h, w] = 0
+            else:
+                img_result[h, w] = 255
+
+    cv2.imwrite('a-robert.bmp', img_result)
 
     return img_result
 
 
 def main():
-    print('1. Reading the image...')
+    print('Reading the image...')
     img = cv2.imread('lena.bmp', cv2.IMREAD_GRAYSCALE)
 
-    img_robert = roberts_operator(img, 30)
-    cv2.imwrite('a-robert.bmp', img_robert)
+    roberts_operator(img, 30)
     
 
 if __name__ == '__main__':
